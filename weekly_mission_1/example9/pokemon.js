6180b2880697da3f9eb8d0ede70d3ba14b18a83d
JohnNadja <cv3802@innovaccion.mx>
2022-04-14 13:48:21 -0500

    Añadiendo example9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,15 @@
+class Pokemon{
+    constructor(name) {
+        this.name = name
+      }
+      sayHello() {
+        console.log(`Mi Pokemon: ${this.name} te saluda :D`)
+      }
+    
+      sayMessage (msg) {
+        console.log(`Mi pokemon dice: ${this.name}] dice: ${msg}`)
+      }
+    
+}
+
+module.exports = Pokemon
