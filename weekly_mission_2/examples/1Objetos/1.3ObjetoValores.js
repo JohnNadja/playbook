e1fee22876fa1d54ede8c1417e1ce3bb0b8b4c61
JohnNadja <cv3802@innovaccion.mx>
2022-04-17 17:35:15 -0500

    Adding examples folder

---
+++ weekly_mission_2/examples/1Objetos/1.3ObjetoValores.js
@@ -0,0 +1,15 @@
+// Un objeto es un conjunto de valores key-value
+const explorer = {
+    firstName: 'Carlo',
+    lastName: 'Gilmar',
+    age: 27,
+    country: 'Mexico',
+    city: 'CDMX',
+    skills: [
+      'JavaScript',
+      'TypeScript',
+      'CoffeeScript',
+      'Elm'
+    ]
+  }
+  console.log(explorer)
---
+++ weekly_mission_2/examples/1Objetos/1.4AccesoPropiedadObjeto.js
@@ -0,0 +1,14 @@
+// Acceder a una propiedad del objeto
+const commander = {
+    firstName: 'Carlo',
+    lastName: 'Gilmar',
+    age: 27,
+    country: 'Mexico',
+    city: 'CDMX',
+    skills: [
+      'JavaScript',
+      'TypeScript',
+      'CoffeeScript',
+      'Elm'
+    ],
+  }
