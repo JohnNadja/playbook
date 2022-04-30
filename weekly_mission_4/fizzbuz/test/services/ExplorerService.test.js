84913e782d2b2ccf80853e54ae0221bec900897b
--oneline <cv3802@innovaccion.mx>
2022-04-30 17:21:36 -0500

    Moviendo archivos a nuevo repositorio

--- weekly_mission_4/fizzbuz/test/services/ExplorerService.test.js
+++
@@ -1,9 +0,0 @@
-const ExplorerService = require("./../../lib/services/ExplorerService")
-
-describe("Unit Tests for ExplorerService Class", () => {
-  test('1) Calcular # Explorers en misión Node', () => {
-    const explorers = [{mission: "node"}] 
-    const explorerInNode = ExplorerService.filterByMission(explorers, "node")
-    expect(explorerInNode.length).toBe(1);
-  });
-})
